cc74a6b63a8d64d8f434416b0fe82ea6d6c476c0
kashanmahmood <kashan.mahmood.24@dartmouth.edu>
2022-04-28 16:42:59 -0400

    added redux

--- src/index.js
+++ src/index.js
@@ -2,51 +2,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.scss';
-import {
-  BrowserRouter, Routes, Route, NavLink, useParams,
-} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import App from './components/app';
+import rootReducer from './reducers';
 
-function App(props) {
-  return (
-    <BrowserRouter>
-      <div>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Welcome />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/test/:id" element={<Test />} />
-          <Route path="*" element={<FallBack />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
-}
-function About(props) {
-  return <div> All there is to know about me </div>;
-}
-function Welcome(props) {
-  return <div>Welcome</div>;
-}
-function Nav(props) {
-  return (
-    <nav>
-      <ul>
-        <li><NavLink to="/" exact>Home</NavLink></li>
-        <li><NavLink to="/about">About</NavLink></li>
-        <li><NavLink to="/test/id1">test id1</NavLink></li>
-        <li><NavLink to="/test/id2">test id2</NavLink></li>
-
-      </ul>
-    </nav>
-  );
-}
-function Test(props) {
-  const { id } = useParams();
-  return <div> ID: {id} </div>;
-}
-function FallBack(props) {
-  return <div>URL Not Found</div>;
-}
+const store = configureStore({
+  reducer: rootReducer,
+});
 
 const root = createRoot(document.getElementById('main'));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
